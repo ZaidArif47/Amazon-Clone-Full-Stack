d84c10779c5d1d33b74ad1b0ebf9ad784d4c6fd3
Zaid Arif <zaid.zrf@gmail.com>
2024-06-16 14:58:51 +0530

    Updated Checkout items on 'Add to Cart' and 'delete' from Cart Summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import { cart, deleteFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+updateCartQuantity();
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -105,5 +107,15 @@ document.querySelectorAll('.js-delete-quantity-link').
         deleteFromCart(itemId);
         const container = document.querySelector(`.js-cart-item-container-${itemId}`);
         container.remove();  //updating the HTML after deleting from Cart
+        updateCartQuantity();
     });
 });
+
+function updateCartQuantity() {
+  let cartQuantity = 0; 
+    cart.forEach((value) => {
+        cartQuantity += value.productQuantity;
+    });
+    
+    document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
+}
