139b7037616cda46f9f1e59b027f2e737c17388b
Zaid Arif <zaid.zrf@gmail.com>
2024-12-08 11:14:40 +0530

    created some more tests for orderSummary

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -4,7 +4,11 @@ import { loadFromStorage, cart } from '../../data/cart.js';
 describe('test suite: renderOrderSummary', () => {
 
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productName1 = 'Black and Gray Athletic Cotton Socks - 6 Pairs';
+    const productPrice1 = '$10.90';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'; 
+    const productName2 = 'Intermediate Size Basketball';
+    const productPrice2 = '$20.95';
 
     beforeEach(() => {
         spyOn(localStorage, 'setItem');
@@ -42,6 +46,12 @@ describe('test suite: renderOrderSummary', () => {
         expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
         expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
         expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
+
+        expect(document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual(productName1);
+        expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual(productName2);
+
+        expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toEqual(productPrice1);
+        expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(productPrice2);
     });    
     
     
@@ -54,5 +64,8 @@ describe('test suite: renderOrderSummary', () => {
 
         expect(cart.length).toEqual(1);
         expect(cart[0].productId).toEqual(productId2);     
+
+        expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual(productName2);
+        expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(productPrice2);
     });
 });
