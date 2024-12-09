84de04f27a966cce20d2868b8b7f05f0839ab7ed
chad zaid <machozaid2@gmail.com>
2024-12-09 18:15:20 +0530

    created deleteFromCart test suite for cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { cart, loadFromStorage, addToCart } from "../../data/cart.js";
+import { cart, loadFromStorage, addToCart, deleteFromCart } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
 
@@ -49,6 +49,83 @@ describe('test suite: addToCart', () => {
             productQuantity: 1,
             deliveryOptionId: '1'
         }]));
+    });
+
+});
+
+
+describe('test suite: deleteFromCart', () => {
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+    });
+
+    it('removes an existing product from cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: productId1,
+                    productQuantity: 2,
+                    deliveryOptionId: '1'
+                },
+                {
+                    productId: productId2,
+                    productQuantity: 1,
+                    deliveryOptionId: '2'
+                }
+            ]);
+        });
+        loadFromStorage();
+
+        deleteFromCart(productId1);
+        
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([
+            {
+                productId: productId2,
+                productQuantity: 1,
+                deliveryOptionId: '2'
+            }
+        ]));
+        
+    });
+
+    it('removing a product that is not in the cart, cart is unhinged', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: productId1,
+                    productQuantity: 2,
+                    deliveryOptionId: '1'
+                },
+                {
+                    productId: productId2,
+                    productQuantity: 1,
+                    deliveryOptionId: '2'
+                }
+            ]);
+        });
+        loadFromStorage();
+
+        deleteFromCart('random-id');
+
+        expect(cart.length).toEqual(2);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([
+            {
+                productId: productId1,
+                productQuantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: productId2,
+                productQuantity: 1,
+                deliveryOptionId: '2'
+            }
+        ]));
     });
 
 });
